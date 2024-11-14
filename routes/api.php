f72b8185ae52cd82e7762d8e03d3e983f4641c1b
Selim <selim@ownweb.fr>
2024-11-14 15:32:53 +0100

    ✨(RoutesApi) : add new routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\RealisationController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +12,5 @@ Route::get('/user', function (Request $request) {
 
 
 Route::resource('users', UserController::class);
+Route::resource('tasks', TaskController::class);
+Route::resource('realisations', RealisationController::class);
